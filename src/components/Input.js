02dcdbf2620360551f2fb65bea0bd1367543d195
gokhandogan7 <gdogan3746@gmail.com>
2020-10-13 23:53:22 +0200

    adjust a padding value

--- src/components/Input.js
+++ src/components/Input.js
@@ -13,7 +13,7 @@ const Input = ({holder}) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#eceff2',
-    padding: 10,
+    padding: 9,
     margin: 10,
     borderRadius: 10,
     marginVertical: 17,
